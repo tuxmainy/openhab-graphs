be5414c051dba0f8d43fd4f7e062cd0e9346f8aa
damage <damage@192.168.11.41>
2020-05-11 22:25:50 +0200

    different default charset at different system... awesome..

--- src/de/devloop/rrd4j/graph/Heating.java
+++ src/de/devloop/rrd4j/graph/Heating.java
@@ -21,16 +21,16 @@ public class Heating extends AGraph {
 	public RrdGraph graph() throws IOException {
 		graphDef.setTitle("Heating");
 		
-		addArea("gas", RRDFiles.C_BOILER_GAS_COUNT, Color.ORANGE, "Gas (*" + Const.GAS_IMPULSE + " mÂ³)");
+		addArea("gas", RRDFiles.C_BOILER_GAS_COUNT, Color.ORANGE, "Gas (*" + Const.GAS_IMPULSE + " m³)");
 		
-		addLine("weather_temp", RRDFiles.WEATHER_NOW_TEMP, Color.BLACK, "Temperatur (Â°C)");
+		addLine("weather_temp", RRDFiles.WEATHER_NOW_TEMP, Color.BLACK, "Temperatur (°C)");
 		
-		addLine("heating_forward", RRDFiles.C_BOILER_HEATING_FORWARD, Color.RED, "Heizung Vorlauf (Â°C)");
-		addLine("heating_return", RRDFiles.C_BOILER_HEATING_RETURN, Color.BLUE, "Heizung RÃ¼cklauf (Â°C)");
-		addLine("water_forward", RRDFiles.C_BOILER_WATER_FORWARD, Color.MAGENTA, "Wasser Vorlauf (Â°C)");
-		addLine("water_return", RRDFiles.C_BOILER_WATER_RETURN, Color.CYAN, "Wasser RÃ¼cklauf (Â°C)");
-		addLine("water_in", RRDFiles.C_BOILER_WATER_IN, Color.GREEN, "Frischwasser (Â°C)");
-		addLine("water_circulation", RRDFiles.C_BOILER_WATER_CIRCULATION, Color.GRAY, "Wasser Zirkulation (Â°C)");
+		addLine("heating_forward", RRDFiles.C_BOILER_HEATING_FORWARD, Color.RED, "Heizung Vorlauf (°C)");
+		addLine("heating_return", RRDFiles.C_BOILER_HEATING_RETURN, Color.BLUE, "Heizung Rücklauf (°C)");
+		addLine("water_forward", RRDFiles.C_BOILER_WATER_FORWARD, Color.MAGENTA, "Wasser Vorlauf (°C)");
+		addLine("water_return", RRDFiles.C_BOILER_WATER_RETURN, Color.CYAN, "Wasser Rücklauf (°C)");
+		addLine("water_in", RRDFiles.C_BOILER_WATER_IN, Color.GREEN, "Frischwasser (°C)");
+		addLine("water_circulation", RRDFiles.C_BOILER_WATER_CIRCULATION, Color.GRAY, "Wasser Zirkulation (°C)");
 
 		return generateGraph();
 	}
